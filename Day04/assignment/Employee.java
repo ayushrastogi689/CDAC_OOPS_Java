985d7f2d58767d17c5f86dbd7cb1b2fd6fd9e5e6
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2024-03-20 02:10:02 +0530

    Implemented Employee class of Day04 Assignment: Q2

---
+++ Day04/assignment/Employee.java
@@ -0,0 +1,13 @@
+// 3)Create a class Employee with multiple overloaded methods that have different parameter types 
+//   (e.g., int, double, String). Demonstrate calling each overloaded method with appropriate arguments.
+
+package assignment;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
